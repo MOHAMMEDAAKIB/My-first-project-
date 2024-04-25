cfabc44fd24376f145738d65ac6cfcbaf88f8a56
AAKIB <156922612+MOHAMMEDAAKIB@users.noreply.github.com>
2024-04-25 17:14:56 +0530

    Update crate_a_loan_calculater.py

--- crate_a_loan_calculater.py
+++ crate_a_loan_calculater.py
@@ -1,6 +1,6 @@
 L = int(input("how much loan you get ")) #lone amount 
 i = float (0.05) #interast rate
-n = int (input("what is the number of pyment")) #number of pyment
+n = int (input("what is the number ofpyment")) #number of pyment
 # M = monthly payments 
 M = L*(i *(1+i)*n)/((1+i)*n -1)
 M = round (M,2)
